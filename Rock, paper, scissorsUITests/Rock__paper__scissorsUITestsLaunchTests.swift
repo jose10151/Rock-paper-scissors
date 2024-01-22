98b72a22c0b9a88b7b1229bb32a88343bfc7bfc0
Jose Trujillo <joseh.trujillo11@gmail.com>
2024-01-22 11:25:57 -0500

    Initial Commit

---
+++ Rock, paper, scissorsUITests/Rock__paper__scissorsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Rock__paper__scissorsUITestsLaunchTests.swift
+//  Rock, paper, scissorsUITests
+//
+//  Created by 4d on 1/22/24.
+//
+
+import XCTest
+
+final class Rock__paper__scissorsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
